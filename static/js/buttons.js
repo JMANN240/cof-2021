4ddd2a1770475c325834f6a0f082223137ce321d
ColbyReinhart <colbyreinhart@gmail.com>
2022-01-27 18:16:40 -0500

    Major changes to css styles. Working on additional pictures functionality.

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -1,15 +1,24 @@
 const electron = require("electron");
 const { ipcRenderer } = electron;
 
-application_buttons = document.querySelectorAll("button.application");
+application_buttons = document.querySelectorAll("button.fancy");
 
 for (let [index, button] of application_buttons.entries()) {
-    button.addEventListener("click", () => {
-        ipcRenderer.send("page:change", button.id);
-    });
-
-    setTimeout(() => {
-        button.style.opacity = 1;
-        button.style.animation = "button-in 2s";
-    }, ( index + 1 ) * 200);
+    if (button.classList.contains("application")) {
+        button.addEventListener("click", () => {
+            ipcRenderer.send("page:change", button.id);
+        });
+    }
+    if (button.classList.contains("good")) {
+        setTimeout(() => {
+            button.style.opacity = 1;
+            button.style.animation = "button-in-good 2s";
+        }, ( index + 1 ) * 200);
+    }
+    if (button.classList.contains("bad")) {
+        setTimeout(() => {
+            button.style.opacity = 1;
+            button.style.animation = "button-in-bad 2s";
+        }, ( index + 1 ) * 200);
+    }
 }
