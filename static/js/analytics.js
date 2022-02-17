ff04c494f043beed5deb83d6a656adaa5bd97066
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-02-17 15:11:25 -0500

    wip analytics listeners

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -2,3 +2,29 @@
 
 console.log('running analytics');
 
+function RecordEvent(event) {
+    let result;
+    if (event instanceof MouseEvent) {
+        console.log(event);
+        result = {
+            path: event.path.filter(o => o && o.localName).map(o => o.localName + (o.id ? '#' + o.id : '') + (o.classList ? [...o.classList.values()].map(o2 => '.' + o2) : '').join(''))
+        }
+    } else if (event instanceof CustomEvent) {
+        result = {
+            type: event.type,
+            detail: event.detail,
+        }
+    }
+    if (result) {
+        console.log(JSON.parse(JSON.stringify(result)))
+    } else {
+        console.error(`Cannot record event "${event.type}"; no logic specified.`, event)
+    }
+    
+}
+
+document.addEventListener('click', RecordEvent);
+
+RecordEvent(new CustomEvent("navigation", {
+    detail: location
+}))
