fdbf6f4458f96677adbe568937cc90401ab2ba64
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-02-17 15:17:28 -0500

    added time, path, and page to analytics tracking

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -6,21 +6,36 @@ function RecordEvent(event) {
     let result;
     if (event instanceof MouseEvent) {
         console.log(event);
-        result = {
-            path: event.path.filter(o => o && o.localName).map(o => o.localName + (o.id ? '#' + o.id : '') + (o.classList ? [...o.classList.values()].map(o2 => '.' + o2) : '').join(''))
-        }
+        result = {}
     } else if (event instanceof CustomEvent) {
         result = {
-            type: event.type,
             detail: event.detail,
         }
+    }
+    if (result) {
+        if (result === true) result = {};
+        result.type = event.type;
+        result.path = event.path.filter(o => o && o.localName)
+            .map(o => (
+                o.localName
+                + (o.id ? '#' + o.id : '')
+                + (o.classList ?
+                    [...o.classList.values()].map(o2 => '.' + o2)
+                    : '').join('')
+            ));
+        result.time = {
+            epoc: Date.now(),
+            local: new Date().toLocaleString(),
+            utc: new Date().toUTCString(),
+        }
+        result.page = location.href;
     }
     if (result) {
         console.log(JSON.parse(JSON.stringify(result)))
     } else {
         console.error(`Cannot record event "${event.type}"; no logic specified.`, event)
     }
-    
+
 }
 
 document.addEventListener('click', RecordEvent);
