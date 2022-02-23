f387d98db9ec5e50ee8cb2a366f76023b991f94d
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-02-23 11:41:49 -0500

    created basic analytics server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const port = 5980;
+app.listen(port, () => {
+    console.log('Listening on port', port)
+})
+
+app.post('/analytics/upload', express.json(), (req, res) => {
+    console.log('uploaded analytics', req.body);
+    res.send('uploaded ' + req.body.length)
+})
+
