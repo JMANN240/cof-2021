a938a2f77c385248e554e9eb8d0ea9d400d32a5c
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-02-23 11:46:05 -0500

    added root page to server

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ app.listen(port, () => {
     console.log('Listening on port', port)
 })
 
+app.get('/', (req, res) => {
+    res.send('Choose Ohio First - 2022 - Symplyfy');
+})
+
 app.post('/analytics/upload', express.json(), (req, res) => {
     console.log('uploaded analytics', req.body);
     res.send('uploaded ' + req.body.length)
