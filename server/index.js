feddb6de23c3f72484bbb25beeaa5d8bcb91d03f
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-02-23 12:30:10 -0500

    added mongodb code to analytics

--- server/index.js
+++ server/index.js
@@ -10,8 +10,20 @@ app.get('/', (req, res) => {
     res.send('Choose Ohio First - 2022 - Symplyfy');
 })
 
-app.post('/analytics/upload', express.json(), (req, res) => {
+
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+let db;
+let dbAnalytics;
+client.connect().then(() => {
+    db = client.db('cof-2022');
+    dbAnalytics = db.collection('analytics');
+})
+
+app.post('/analytics/upload', express.json(), async (req, res) => {
     console.log('uploaded analytics', req.body);
+    await dbAnalytics.insertMany(req.body);
     res.send('uploaded ' + req.body.length)
 })
 
