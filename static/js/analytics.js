5b403b4b777dd568e6f6e1bc40e450343d4c89d8
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-02-23 11:34:10 -0500

    upload logic

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -1,6 +1,53 @@
 
+const { ipcRenderer } = require('electron');
+const { v4: uuidv4 } = require('uuid');
+const username = ipcRenderer.invoke("settings:get", "username");
+const userId = new Promise(async function(resolve) {
+    resolve((await username) + ':' + uuidv4());
+});
 
 console.log('running analytics');
+
+const uploadQueue = [];
+async function PushUploadQueue() {
+    if (uploadQueue.length == 0) return;
+    const user = await userId;
+    const items = uploadQueue.splice(0, Math.min(100, uploadQueue.length))
+    .map(o => {
+        o.user = user;
+        return o;
+    });
+    // Queue is not empty. Trigger another upload.
+    if (uploadQueue.length > 0) UploadEvent();
+    try {
+        await fetch('https://host.com/cof-2022/analytics/upload', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(items),
+        })
+    } catch(e) {
+        // Failed to push
+        // Put items back at the beginning of the queue
+        uploadQueue.unshift(...items);
+        // Trigger another upload.
+        UploadEvent();
+    }
+};
+
+// Batched uploading to preserve data & requests
+// Upload once no more events have come in for 2 seconds or until the queue has reached 20 entries
+let batchedUpload;
+function UploadEvent(payload) {
+    if (payload) uploadQueue.push(payload);
+    if (uploadQueue.length > 20) {
+        clearTimeout(batchedUpload);
+        PushUploadQueue();
+    }
+    if (batchedUpload) clearTimeout(batchedUpload);
+    batchedUpload = setTimeout(PushUploadQueue, 2000);
+}
 
 function RecordEvent(event) {
     let result;
@@ -32,6 +79,7 @@ function RecordEvent(event) {
     }
     if (result) {
         console.log(JSON.parse(JSON.stringify(result)))
+        UploadEvent(result);
     } else {
         console.error(`Cannot record event "${event.type}"; no logic specified.`, event)
     }
